1385099d1c8732bca49361de7d873cf9f914859d
robs-am <roberta.amaro89@gmail.com>
2022-10-25 01:08:21 -0300

    finalizando backend

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-import express from "express"
+import express from "express";
+import useRoutes from "./routes/users.js";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.listen(8880)
+app.use("/", useRoutes); //para o caminhao da pasta raiz, será usado o useRoutes
+
+app.listen(8800);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { getUsers } from "../controllers/user.js";
 
 const router = express.Router();
 
