bd2289cbeb76cc5132480c0ad70ab6eac5df8ecd
robs-am <roberta.amaro89@gmail.com>
2022-10-26 08:12:46 -0300

    add form styled.form

--- frontend/src/components/Form/form.jsx
+++ frontend/src/components/Form/form.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const FormContainer = styled.form `
+display: flex;
+align-items: flex-end;
+gap: 18px;
+flex-wrap: wrap;
+background-color: #fff;
+padding: 20px;
+box-shadow: 0px 0px 5px #ccc; 
+border-radius:5px; 
+`;
 
 const form = () => {
   return (
