d31c2e1e8266ac0f87d3c6d7eb0d7be9effdee98
robs-am <roberta.amaro89@gmail.com>
2022-10-26 14:48:01 -0300

    add input, label styled

--- frontend/src/components/Form/form.jsx
+++ frontend/src/components/Form/form.jsx
@@ -20,6 +20,16 @@ display: flex;
 flex-direction: column;
 `;
 
+const Input = styled.input`
+width: 120px;
+padding: 0 10px;
+border: 1px solid #bbb;
+border-radius : 5px;
+height: 40px;
+`;
+
+const Label = styled.label``;
+
 
 const Form = ({onEdit}) => {
     const ref = useRef();
