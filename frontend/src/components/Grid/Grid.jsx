6ed46be05236251aa4258d521f15caecc4a21127
robs-am <roberta.amaro89@gmail.com>
2022-10-26 16:03:42 -0300

    add grid tyled

--- frontend/src/components/Grid/Grid.jsx
+++ frontend/src/components/Grid/Grid.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Table = styled.table`
+width: 100%;
+background-color: #fff;
+padding: 20px;
+box-shadow: Opx Opx 5px #ccc;
+border-radius: 5px;
+max-width: 808px;
+margin: 20px auto;
+word-break: break-all;
+`;
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <div></div>
   )
 }
 
