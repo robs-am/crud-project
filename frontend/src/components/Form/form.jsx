677759c420cea8208aad244b02324ae987dc5d98
robs-am <roberta.amaro89@gmail.com>
2022-10-26 08:17:51 -0300

    add formcontainer

--- frontend/src/components/Form/form.jsx
+++ frontend/src/components/Form/form.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-const FormContainer = styled.form `
-display: flex;
-align-items: flex-end;
-gap: 18px;
-flex-wrap: wrap;
-background-color: #fff;
-padding: 20px;
-box-shadow: 0px 0px 5px #ccc; 
-border-radius:5px; 
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 18px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
 `;
 
-const form = () => {
+const Form = () => {
   return (
-    <div>form</div>
-  )
-}
+    <div>
+      <FormContainer>
+        
+      </FormContainer>
+    </div>
+  );
+};
 
-export default form
+export default Form;
