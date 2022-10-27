c245b85d4555f666f7a371efbf5c0ea7ea27b29e
robs-am <roberta.amaro89@gmail.com>
2022-10-27 07:22:11 -0300

    add funcao assincrona

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import Global from "./styles/global";
 import styled from "styled-components";
+import axios from "axios";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -26,7 +28,18 @@ const Title = styled.h2 ``;
 function App() {
   const [users, setUsers] = useState([]);
   const [onEdit, setOnEdit] = useState(null);
-  
+
+  //função recebe assincronamente infos do banco de dados//
+
+  const getUsers = async() => {
+    try{
+      const res = await axios.get("localhost:8800"); //response, await and axios fazendo um GET
+      setUsers(res.data.sort((a,b) =>(a.nome > b.nome ? -1 :1))); //sort, sorteará por ordem alfabética
+    } catch (error) {
+      toast.error(error);  //pop-up toast de error
+    }
+  };
+
   return (
     <>
     <Container>
